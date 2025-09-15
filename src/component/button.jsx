1f00575af74a5938b8285f54f65845c675cdaa5f
이윤지 <yoonji0879@dgsw.hs.kr>
2025-09-15 23:48:47 +0900

    버튼 기본 크기 지정

--- src/component/button.jsx
+++ src/component/button.jsx
@@ -1,3 +1,11 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  width: 6.4375rem;
+  height: 2.5625rem;
+  padding: 0.6875rem 0.9375rem;
+  `
+
 function Button( {children, onClick, variant = primary, ...props}) {
   return (
     <button onClick={onClick}
