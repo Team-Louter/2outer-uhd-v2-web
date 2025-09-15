a9a72c336dcdd0c87794e87b51aa7cbe42afdc8f
조상철 <chakanchul2533@gmail.com>
2025-09-15 19:26:10 +0900

    feat: assets 요소 추가 및 jsx, css 파일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Index from './pages/Index';
+import Intro from './pages/Intro';
+import Find from './pages/Find';
+import Found from './pages/Found';
+import FindItRegister from './pages/FindItRegister';
+import FoundRegister from './pages/FoundRegister';
+import SignIn from './pages/SignIn';
+import SignupID from './pages/SignupID';
+import SignupPW from './pages/SignupPW';
+import SignupSchool from './pages/SignupSchool';
+import MyPost from './pages/MyPost';
+import Popup from './pages/Popup';
+import './styles/reset.css';  // 이 파일이 존재하는지 확인
 
 function App() {
   return (
-    <>
-
-    </>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/intro" element={<Intro />} />
+          <Route path="/find" element={<Find />} />
+          <Route path="/found" element={<Found />} />
+          <Route path="/findit-register" element={<FindItRegister />} />
+          <Route path="/found-register" element={<FoundRegister />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup-id" element={<SignupID />} />
+          <Route path="/signup-pw" element={<SignupPW />} />
+          <Route path="/signup-school" element={<SignupSchool />} />
+          <Route path="/mypost" element={<MyPost />} />
+          <Route path="/popup" element={<Popup />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
