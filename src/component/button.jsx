486197e63478e38dba9a25d1a7aa76f3bb36d28c
이윤지 <yoonji0879@dgsw.hs.kr>
2025-09-16 11:02:38 +0900

    style: 버튼 컴포넌트 스타일 지정

--- src/component/button.jsx
+++ src/component/button.jsx
@@ -1,18 +1,38 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
-const StyledButton = styled.button`
+const commonButtonStyle = css `
   width: 6.4375rem;
   height: 2.5625rem;
   padding: 0.6875rem 0.9375rem;
+  cursor: pointer;
+  border-radius: 10px;
   `
 
-function Button( {children, onClick, variant = primary, ...props}) {
+const buttonVariant = {
+  primary: css `
+  background-color: #1A1E47;
+  color: #fff;
+  border: none;
+  `, 
+  secondary: css`
+  background-color: #fff;
+  color: #1A1E47;
+  border: solid 1px #808080;
+  `
+}
+
+const StyledButton = styled.button`
+  ${commonButtonStyle}
+  ${props => buttonVariant[props.variant || 'primary']}
+  `
+
+function Button( {children, onClick, variant = 'primary', ...props}) {
   return (
-    <button onClick={onClick}
-    className={`button ${variant}`}
+    <StyledButton onClick={onClick}
+    variant={variant}
     {...props}>
       {children}
-    </button>
+    </StyledButton>
   )
 }
 
---
+++ src/pages/Index.jsx
@@ -0,0 +1,13 @@
+import Button from "../component/button";
+
+function Index() {
+  return (
+    <div>
+      <h3>test</h3>
+      <Button>기본 버튼</Button>
+      <Button variant="secondary">보조 버튼</Button>
+    </div>
+  );
+}
+
+export default Index;
