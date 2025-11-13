91db47602f058c099b1d8534fd1735edbcebf5bf
조상철 <chakanchul2533@gmail.com>
2025-11-13 23:33:40 +0900

    feat: 게시물 상세 페이지 파일 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Index from './pages/Index';
-import Intro from './pages/Intro';
-import Find from './pages/Find';
-import Found from './pages/Found';
-import FindItRegister from './pages/FindItRegister';
-import FoundRegister from './pages/FoundRegister';
-import SignIn from './pages/SignIn';
-import SignupID from './pages/SignupID';
-import SignupPW from './pages/SignupPW';
-import SignupSchool from './pages/SignupSchool';
-import MyPost from './pages/MyPost';
-import Popup from './pages/Popup';
-import MyPage from './pages/MyPage';
-import GlobalStyle from './styles/GlobalStyle';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Intro from "./pages/Intro";
+import Find from "./pages/Find";
+import Found from "./pages/Found";
+import FindItRegister from "./pages/FindItRegister";
+import FoundRegister from "./pages/FoundRegister";
+import SignIn from "./pages/SignIn";
+import SignupID from "./pages/SignupID";
+import SignupPW from "./pages/SignupPW";
+import SignupSchool from "./pages/SignupSchool";
+import MyPost from "./pages/MyPost";
+import Popup from "./pages/Popup";
+import MyPage from "./pages/MyPage";
+import GlobalStyle from "./styles/GlobalStyle";
+import PostDetail from "./pages/PostDetail";
 
 function App() {
   return (
     <>
-    <GlobalStyle />
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/intro" element={<Intro />} />
-          <Route path="/find" element={<Find />} />
-          <Route path="/found" element={<Found />} />*/}
-          <Route path="/findit-register" element={<FindItRegister />} />
-          <Route path="/found-register" element={<FoundRegister />} />
-          {/*<Route path="/signin" element={<SignIn />} />
+      <GlobalStyle />
+      <Router>
+        <div className='App'>
+          <Routes>
+            <Route path='/' element={<Index />} />
+            <Route path='/intro' element={<Intro />} />
+            <Route path='/find' element={<Find />} />
+            <Route path='/found' element={<Found />} />
+            <Route path='/findit-register' element={<FindItRegister />} />
+            <Route path='/found-register' element={<FoundRegister />} />
+            {/*<Route path="/signin" element={<SignIn />} />
           <Route path="/signup-id" element={<SignupID />} />
           <Route path="/signup-pw" element={<SignupPW />} />
           <Route path="/signup-school" element={<SignupSchool />} />*/}
-          <Route path="/mypost" element={<MyPost />} />
-          <Route path="/popup" element={<Popup />} />
-          <Route path="/mypage" element={<MyPage />} />
-        </Routes>
-      </div>
-    </Router>
+            <Route path='/mypost' element={<MyPost />} />
+            <Route path='/popup' element={<Popup />} />
+            <Route path='/mypage' element={<MyPage />} />
+            <Route path='/post-detail' element={<PostDetail />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
---
+++ src/pages/PostDetail.jsx
@@ -0,0 +1,12 @@
+import Header from "../component/organisms/header";
+import S from '../styles/post-detail';
+
+function SignIn() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default SignIn;
